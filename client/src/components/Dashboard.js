981c69f9444ad84c53b60456ca17289cca029e9b
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-11-13 14:52:55 -0300

    Song search initial configuration

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react"
 import useAuthenticationHelper from "../hooks/useAuthenticationHelper"
+import SpotifyWebApi from "spotify-web-api-node"
+import axios from "axios"
 
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: "d2a7d543ee8141ee9e85e54c63fdd6e3",
+})
 const Dashboard = ({code}) => {
-
+    const [search, setSearch] = useState("queen")
+    const [searchResults, setSearchResults] = useState([])
     const accessToken = useAuthenticationHelper(code)
+
+
+    useEffect(() => {
+        if (!accessToken) return 
+        spotifyApi.setAccessToken(accessToken.accessToken)
+    }, [accessToken])
+    
+    useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken.accessToken) return
+        console.log("searc", search)
+        console.log("otek", accessToken)
+        
+        spotifyApi.searchTracks(search).then(res => {
+            console.log("res", res)
+        })
+    }, [search, accessToken])
 
     return (
         <div>
-            {code}
+            <form>
+                <input type="text" placeholder="buscar cancion" onChange={e => setSearch(e.target.value)} />
+                <button >buscar</button>
+            </form>
         </div>
     )
 }
--- client/src/hooks/useAuthenticationHelper.js
+++ client/src/hooks/useAuthenticationHelper.js
@@ -8,22 +8,41 @@ export function useAuthenticationHelper (code) {
     const [expiresIn, setExpiresIn] = useState()
 
     useEffect(() => {
-        axios
-          .post("http://localhost:3001/login", {
-            code,
-          })
-          .then(res => {
-              console.log("res", res.data)
-            // setAccessToken(res.data.accessToken)
-            // setRefreshToken(res.data.refreshToken)
-            // setExpiresIn(res.data.expiresIn)
-            // window.history.pushState({}, null, "/")
-          })
-          .catch((err) => {
-            console.log(err)
-            window.location = "/"
-          })
+      axios
+        .post("http://localhost:3001/login", {
+          code,
+        })
+        .then(res => {
+          console.log("datos", res)
+          setAccessToken(res.data.accessToken)
+          setRefreshToken(res.data.refreshToken)
+          setExpiresIn(res.data.expiresIn)
+          window.history.pushState({}, null, "/")
+        })
+        .catch((err) => {
+          console.log(err)
+          window.location = "/"
+        })
       }, [code])
+
+      useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+          axios
+            .post("http://localhost:3001/refresh", {
+              refreshToken,
+            })
+            .then(res => {
+              setAccessToken(res.data.accessToken)
+              setExpiresIn(res.data.expiresIn)
+            })
+            .catch(() => {
+              window.location = "/"
+            })
+        }, (expiresIn - 60) * 1000)
+    
+        return () => clearInterval(interval)
+      }, [refreshToken, expiresIn])
 
     return {
         accessToken
