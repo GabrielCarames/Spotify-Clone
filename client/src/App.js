8d96c47b82c643b21fa8fad4244fd79587fa6abe
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-08-26 15:49:07 -0300

    netlify

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch   } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import useAuthenticationHelper from "./hooks/useAuthenticationHelper";
 import YourLibrary from "./components/YourLibrary";
@@ -9,32 +9,32 @@ import Songs from "./components/Songs";
 import Main from "./components/Main";
 
 const mapStateToProps = (state) => {
-    return { results: state }
+  return { results: state }
 }
 
 function App() {
-    const code = new URLSearchParams(window.location.search).get("code")
-    const userLogged = JSON.parse(localStorage.getItem('userLogged'))
-    useAuthenticationHelper(code)
+  const code = new URLSearchParams(window.location.search).get("code")
+  const userLogged = JSON.parse(localStorage.getItem('userLogged'))
+  useAuthenticationHelper(code)
 
-    return (
-        userLogged !== null ? 
-                <Router>
-                    <div className="app-container">
-                        <Main accessToken={userLogged.accessToken} > </Main>
-                        <Switch>
-                            <Route exact path="/"><YourLibrary/></Route>
-                            <Route exact path="/search"><Search/></Route>
-                            <Route exact path="/search/category/:categoryName"><Category/></Route>
-                            <Route exact path="/search/:search"><Results/></Route>
-                            <Route exact path="/yourlibrary"><YourLibrary/></Route>
-                            <Route exact path="/playlist/:playlistId"><Songs/></Route>
-                            <Route path="*"><YourLibrary/></Route>
-                        </Switch>
-                    </div>
-                </Router>
-        :   window.location.href = "https://accounts.spotify.com/authorize?client_id=d2a7d543ee8141ee9e85e54c63fdd6e3&response_type=code&redirect_uri=https://gabriel-carames-spotify-clone.herokuapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
-    )
+  return (
+    userLogged !== null ?
+      <Router>
+        <div className="app-container">
+          <Main accessToken={userLogged.accessToken} > </Main>
+          <Switch>
+            <Route exact path="/"><YourLibrary /></Route>
+            <Route exact path="/search"><Search /></Route>
+            <Route exact path="/search/category/:categoryName"><Category /></Route>
+            <Route exact path="/search/:search"><Results /></Route>
+            <Route exact path="/yourlibrary"><YourLibrary /></Route>
+            <Route exact path="/playlist/:playlistId"><Songs /></Route>
+            <Route path="*"><YourLibrary /></Route>
+          </Switch>
+        </div>
+      </Router>
+      : window.location.href = "https://accounts.spotify.com/authorize?client_id=d2a7d543ee8141ee9e85e54c63fdd6e3&response_type=code&redirect_uri=https://spotify-clone-gabriel-carames.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  )
 }
 
 export default connect(mapStateToProps)(App);
--- client/src/hooks/useSongsHelper.js
+++ client/src/hooks/useSongsHelper.js
@@ -3,60 +3,60 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import SpotifyWebApi from "spotify-web-api-node"
 
-export function useSongsHelper (dispatch) {
-    const accessToken = JSON.parse(localStorage.getItem('userLogged')).accessToken
-    const [playlist, setPlaylist] = useState()
-    const { playlistId } = useParams()
-    const likedSongs = useSelector(state => state.likedSongsReducer)
-    const spotifyApi = new SpotifyWebApi({clientId: "d2a7d543ee8141ee9e85e54c63fdd6e3"})
+export function useSongsHelper(dispatch) {
+  const accessToken = JSON.parse(localStorage.getItem('userLogged')).accessToken
+  const [playlist, setPlaylist] = useState()
+  const { playlistId } = useParams()
+  const likedSongs = useSelector(state => state.likedSongsReducer)
+  const spotifyApi = new SpotifyWebApi({ clientId: "d2a7d543ee8141ee9e85e54c63fdd6e3" })
 
-    useEffect(() => {
-        if(playlistId !== "likedsongs") {
-            spotifyApi.setAccessToken(accessToken)
-            spotifyApi.getPlaylist(playlistId).then(function(data) {
-                setPlaylist(data.body)
-            }, function(err) {
-                console.log('Something went wrong!', err);
-            });
-        } else setPlaylist(likedSongs)
-    }, [accessToken])
+  useEffect(() => {
+    if (playlistId !== "likedsongs") {
+      spotifyApi.setAccessToken(accessToken)
+      spotifyApi.getPlaylist(playlistId).then(function (data) {
+        setPlaylist(data.body)
+      }, function (err) {
+        console.log('Something went wrong!', err);
+      });
+    } else setPlaylist(likedSongs)
+  }, [accessToken])
 
-    useEffect(() => {
-        window.onload = function () {
-            const observer = new IntersectionObserver( 
-                ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
-                {threshold: [1]}
-            );
-            observer.observe(document.getElementsByClassName('songs-complementary')[0]);
-        }
-    }, [])
-
-    const millisToMinutesAndSeconds = (millis) => {
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0);
-        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  useEffect(() => {
+    window.onload = function () {
+      const observer = new IntersectionObserver(
+        ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
+        { threshold: [1] }
+      );
+      observer.observe(document.getElementsByClassName('songs-complementary')[0]);
     }
+  }, [])
 
-    const defineClassName = (isPlaying, uri, itemUri, className) => {
-        if(isPlaying) {
-            if(uri === itemUri) return className + "--active"
-            else return className
-        } else return className
+  const millisToMinutesAndSeconds = (millis) => {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+
+  const defineClassName = (isPlaying, uri, itemUri, className) => {
+    if (isPlaying) {
+      if (uri === itemUri) return className + "--active"
+      else return className
+    } else return className
+  }
+
+  const playSong = (songUri, songItemUri) => {
+    if (songUri === songItemUri) {
+      dispatch({ type: '@songState', payload: true })
+    } else dispatch({ type: '@setSong', payload: songItemUri })
+  }
+
+  const stopSong = (songUri, songItemUri) => {
+    if (songUri === songItemUri) {
+      dispatch({ type: '@songState', payload: false })
     }
+  }
 
-    const playSong = (songUri, songItemUri) => { 
-        if(songUri === songItemUri) {
-            dispatch({type: '@songState', payload: true})
-        } else dispatch({type: '@setSong', payload: songItemUri})
-    }
-
-    const stopSong = (songUri, songItemUri) => { 
-        if(songUri === songItemUri) {
-            dispatch({type: '@songState', payload: false})
-        }
-    } 
-
-    return { playlist, millisToMinutesAndSeconds, defineClassName, playSong, stopSong }
+  return { playlist, millisToMinutesAndSeconds, defineClassName, playSong, stopSong }
 }
 
 export default useSongsHelper
