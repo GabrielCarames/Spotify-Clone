de02d96bacb3738538bf38a66b30b4aeb41375c2
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-08-26 18:36:23 -0300

    test3

--- app.js
+++ app.js
@@ -9,45 +9,45 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post("/login", (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
     })
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in,
-        })
-    }).catch(err => {
-        console.log("err", err)
-        res.sendStatus(400)
-    })
+  }).catch(err => {
+    console.log("err", err)
+    res.sendStatus(400)
+  })
 })
 
 app.post("/refresh", (req, res) => {
-    const refreshToken = req.body.refreshToken
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken,
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
+  spotifyApi.refreshAccessToken().then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      expiresIn: data.body.expires_in,
     })
-    spotifyApi.refreshAccessToken().then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            expiresIn: data.body.expires_in,
-        })
-    }).catch(err => {
-        console.log(err)
-        res.sendStatus(400)
-    })
+  }).catch(err => {
+    console.log(err)
+    res.sendStatus(400)
+  })
 })
-
+console.log("server iniciado")
 app.listen(process.env.PORT || 3001)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
